efd17cf58c7a803cf432acfb4bd05bbb3f36e2e6
Yoshida010 <kamui0923@ezweb.ne.jp>
2022-11-28 19:23:51 +0900

    Add login and user management page

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,11 +4,13 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { User } from "../types/api/user";
+import { useLoginUser } from "./useLoginUser";
 import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
 	const navigate = useNavigate();
 	const { showMessage } = useMessage();
+	const { setLoginUser } = useLoginUser();
 
 	const [loading, setLoading] = useState(false);
 
@@ -19,6 +21,8 @@ export const useAuth = () => {
 			.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
 			.then((res) => {
 				if (res.data) {
+					const isAdmin = res.data.id === 10 ? true : false;
+					setLoginUser({ ...res.data, isAdmin });
 					showMessage({ title: "ログインしました", status: "success" });
 					navigate("/home");
 				} else {
@@ -33,5 +37,5 @@ export const useAuth = () => {
 			});
 	}, []);
 
-	return { login, loading };
+	return { login, loading, setLoginUser };
 };
