c60ce2d300c141f29b76917d3d0dbaee4963aea8
Yoshida010 <kamui0923@ezweb.ne.jp>
2022-11-28 17:02:51 +0900

    Add button component

---
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -0,0 +1,20 @@
+/** @format */
+
+import type { FC, ReactNode } from "react";
+import { Button } from "@chakra-ui/react";
+
+type Props = {
+	children: ReactNode;
+	disabled?: boolean;
+	loading?: boolean;
+	onClick: () => void;
+};
+
+export const PrimaryButton: FC<Props> = (props) => {
+	const { children, disabled = false, loading = false, onClick } = props;
+	return (
+		<Button bg="teal.400" color="white" _hover={{ opacity: "0.8" }} onClick={onClick} isLoading={loading} disabled={disabled || loading}>
+			{children}
+		</Button>
+	);
+};
